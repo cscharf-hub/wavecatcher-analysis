cf2497d7180af1712b3b132ce8993ffde1acbafc
Christian Scharf <83631961+cscharf-hub@users.noreply.github.com>
2025-03-12 17:57:22 +0100

    Update install_miniconda.sh
    Fixed automatic cleaning of temporary files

--- etc/scripts/install_miniconda.sh
+++ etc/scripts/install_miniconda.sh
@@ -10,19 +10,19 @@ PLATFORM=$(uname)
 ARCH=$(uname -m)
 
 if [[ $ARCH!="x86_64" ]] && [[ $ARCH!="x86" ]]; then
-    printf "INFO: Your system architecture (%s) might not be fully supported\n" $(uname -m)
+    printf "INFO: Your system architecture is (%s)\n" $(uname -m)
     printf "In case of problems please follow the instructions at https://github.com/conda-forge/miniforge/n/n" 
     
     curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
     bash Miniforge3-$PLATFORM-$ARCH.sh
     source $HOME/.bashrc
-    rm Miniforge3-$PLATFORM-$ARCH.sh
+    rm -f Miniforge3-$PLATFORM-$ARCH.sh
 else
     wget https://repo.anaconda.com/miniconda/Miniconda3-$MINICONDA_VERSION-$PLATFORM-$ARCH.sh -O miniconda.sh
     bash miniconda.sh -b -p $HOME/miniconda
     echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> $HOME/.bashrc
     source $HOME/.bashrc
-    rm miniconda.sh
+    rm -f miniconda.sh
 fi
 
 # Initialize the base environment
